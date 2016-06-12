a6fe2443535b926299539581452519c49d5ccacb
Abraham Arce <xe1gyq@gmail.com>
2016-06-12 13:19:17 -0500

    Update Name

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 echo "Welcome to Grove Indoor Environment Kit Intelligent System"
 
-git clone https://github.com/xe1gyq/giekis.git
-cd giekis/
+git clone https://github.com/xe1gyq/GiekIs.git
+cd GiekIs/
 pip install -r requirements.pip
 sh requirements.opkg
 git clone https://github.com/xe1gyq/core.git
 
-echo "Now go to giekis directory to get started!"
+echo "Now go to GiekIs directory to get started!"
 echo "First work on having your credentials ready as described under"
 echo "https://xe1gyq.gitbooks.io/core/content/documentation/Credentials.html"
-echo "under giekis/core/configuration/credentials.config"
+echo "under GiekIs/core/configuration/credentials.config"
 
-echo "Happy giekis'ing"
+echo "Happy GiekIs'ing"
