ca5ac1aeeae3b2d6b06c6c1acf05015bc475ce21
Abraham Arce <xe1gyq@gmail.com>
2016-06-12 18:29:27 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- setup.sh
+++ setup.sh
@@ -6,9 +6,14 @@ pip install -r requirements.pip
 sh requirements.opkg
 git clone https://github.com/xe1gyq/core.git
 
+echo
 echo "Now go to GiekIs directory to get started!"
+echo
 echo "First work on having your credentials ready as described under"
 echo "https://xe1gyq.gitbooks.io/core/content/documentation/Credentials.html"
-echo "under GiekIs/core/configuration/credentials.config"
-
+echo "under GiekIs/core/configuration/"
+echo
+echo "The following files need to be present"
+echo "credentials.config  voicerss.ak voicerss.mk"
+echo
 echo "Happy GiekIs'ing"
