73f885178d3de70b07494d78db6e6169c24b43ed
Abraham Arce <xe1gyq@gmail.com>
2016-08-13 14:27:27 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- setup.sh
+++ setup.sh
@@ -10,11 +10,13 @@ git clone https://github.com/xe1gyq/core.git
 echo
 echo "Now go to GiekIs directory to get started!"
 echo
-echo "First work on having your credentials ready as described under"
+echo "Work on having your credentials ready as described under"
 echo "https://xe1gyq.gitbooks.io/core/content/documentation/Credentials.html"
-echo "under GiekIs/core/configuration/"
 echo
-echo "The following files need to be present"
-echo "credentials.config  voicerss.ak voicerss.mk"
+echo "The following files need to be modified"
+echo
+echo "core/configuration/credentials.config"
+echo "core/configuration/voicerss.ak"
+echo "core/configuration/voicerss.mk"
 echo
 echo "Happy GiekIs'ing"
